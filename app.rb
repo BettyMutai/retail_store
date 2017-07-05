d8c81ff47e9a387ef2cb8a5475e50c070fc066d6
kitet <kitetnet@gmail.com>
2017-07-05 11:13:59 +0300

    no error on compilation but error on logic

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require './lib/class.rb'
+also_reload('lib/**/*.rb')
+
+get('/') do
+	erb(:index)
+end
+
+post ('/') do
+	sales=Sales.new()
+	@sku=params['sku']
+	myhash=sales.getFinalResults(@sku)
+	@totalsale=myhash.fetch("firsthash").fetch("total_KSH")
+	@topstore=myhash.fetch("secondhash").fetch("topstore")
+	erb(:index)
+end
---
+++ lib/class.rb
@@ -0,0 +1,55 @@
+require 'csv'
+
+class Sales
+	@@allsales=[]
+	@@searcharray=[]
+
+	#create array from all the file rows
+	def get_allsales()
+		CSV.foreach('sales.csv') do |row|
+			@@allsales.push(row)
+		end
+	end
+	#search through all sales to get row with the sku
+	def search(sku)
+		@@allsales.each() do |row|
+			#row=["Nairobi", DM1759, "6900 KSH"]
+			if row[1].eql?(sku)
+				@@searcharray.push(row)
+			end
+		end
+	end
+	#get total sales of a single sku
+	def getFinalResults(sku)
+		get_allsales()
+		search(sku)
+		@totalsales=0
+		@highestvalue=0
+		@highsellingstore=""
+		@@searcharray.each() do |storesale|
+			#storesale=["Nairobi", "DM1000","70000      KSH"]
+			#sub any extra space
+			salevalue=(storesale[2].gsub(/\s+/, " ").split(" "))[0].to_i
+			@totalsales+=salevalue
+			#get highest selling store
+			if salevalue>@highestvalue
+				@highestvalue=salevalue
+				@highsellingstore=storesale[0]
+			end
+		end
+		#to return value in hash
+		saleshash=Hash.new()
+		saleshash.store("total_KSH", @totalsales)
+		
+		#highest selling store and selling value
+		secondhash=Hash.new()
+		secondhash.store("topstore",@highsellingstore)
+
+		finalhash=Hash.new()
+		finalhash.store("firsthash",saleshash)
+		finalhash.store("secondhash",secondhash)
+
+		finalhash
+	end
+end
+
